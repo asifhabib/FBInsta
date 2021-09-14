b29b3aaac6098f684e86455b25cb9f4e6a9d79fe
Asif Habib <asif.habib@purelogic.net>
2021-09-14 15:02:09 +0500

    - Feed code moved to feedVC

--- FBInsta/FBFeedViewController.swift
+++ FBInsta/FBFeedViewController.swift
@@ -27,3 +27,70 @@ class FBFeedViewController: UIViewController {
     */
 
 }
+//class ViewController: UIViewController {
+//
+//    @IBOutlet weak var btn_login : UIButton!
+//    @IBOutlet weak var btn_fetchFeed : UIButton!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        setupFacebook()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//    func setupFacebook(){
+//        if !isLoggedIn() {
+//            btn_login.setTitle("Login Facebook", for: .normal)
+//            return
+//        }
+//        btn_login.setTitle("Logout Facebook", for: .normal)
+//        proceedWithGraphRequest()
+//
+//    }
+//    func isLoggedIn() -> Bool {
+//        if let token = AccessToken.current,
+//           token.isExpired == false {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    @IBAction func onClick_login(_ sender : Any){
+//        if isLoggedIn() {
+//            LoginManager().logOut()
+//            AccessToken.current = nil
+//            return
+//        }
+//
+//        LoginManager().logIn(permissions: ["email","public_profile","user_posts"], from: self) { [weak self] (result, error) in
+//            if let error = error {
+//                print("Login Failed with Error :\(error)")
+//            }
+//            else {
+//                self?.proceedWithGraphRequest()
+//            }
+//        }
+//
+//
+//
+//    }
+//    func proceedWithGraphRequest(){
+//        let request = GraphRequest(graphPath: "/me", parameters: ["fields": "id,name,email,posts"], httpMethod: .get)
+//
+//        request.start { (connection, result, error) in
+//            if let error = error {
+//                print("GraphRequest Error :\(error)")
+//            }
+//            else {
+//                print("Response :\(result)")
+//            }
+//        }
+//
+//
+//    }
+//    @IBAction func onClick_fetchFeed(_ sender : Any){
+//
+//    }
+//}
